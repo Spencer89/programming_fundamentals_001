51f2388695aa9279183dc8fb30c296f5567c0b56
Nichole Spencer <nic_spencer@hotmail.co.uk>
2020-03-27 19:23:14 +0000

    Initial Commit

--- app/bookshop_utils.js
+++ app/bookshop_utils.js
@@ -2,6 +2,37 @@ function addVAT(price) {
     return price + (0.2 * price);
 }
 
+function getFullName(firstName, lastName) {
+    return `${firstName} ${lastName}`;
+}
+
+function getHalfPrice(price) {
+    return (price/2);
+}
+
+function countBooks(array){
+    return (array.length);
+}
+
+function isInStock(book) {
+    if (book.quantity > 0)
+    {return true}
+    else {return false};
+}
+
+function getTotalOrderPrice(price, quantity){
+let orderTotal = price * quantity;
+let vatTotal = orderTotal + (0.2 * orderTotal);
+return vatTotal
+
+;}
+
+
 module.exports = {
-    addVAT
+    addVAT,
+    getFullName,
+    getHalfPrice,
+    countBooks,
+    isInStock,
+    getTotalOrderPrice
 };
--- test/utils.test.js
+++ test/utils.test.js
@@ -8,4 +8,58 @@ describe("utils.addVAT", () => {
   test("returns 120 when passed 100", () => {
     expect(utils.addVAT(100)).toBe(120);
   });
+
+  test("returns 118.8 when passed 99", () => {
+    expect(utils.addVAT(99)).toBe(118.8);
+  });
 });
+
+describe("utils.getFullName", () => {
+  test("returns Haruki Murakami when passed Haruki and Murakami", () => {
+    expect(utils.getFullName("Haruki", "Murakami")).toBe("Haruki Murakami");
+  });
+});
+
+describe("utils.getHalfPrice", () => {
+  test("returns 10 when passed 20", () => {
+    expect(utils.getHalfPrice(20)).toBe(10);
+  });
+});
+
+describe("utils.countBooks", () => {
+  test("return total number of books", () => {
+    expect(utils.countBooks(["1", "2", "3"])).toBe(3);
+  });
+}); 
+
+describe("utils.isInStock", () =>{
+  test("returns true if the title is in stock", () => {
+  const book = {
+    title: "The Stone Diaries",
+    author: "Carol Shields",
+    yearOfPublication: 1993,
+    quantity: 9
+  };
+
+  expect(utils.isInStock(book)).toBe(true);
+});
+});
+
+describe("utils.isInStock", () =>{
+  test("returns true if the title is in stock", () => {
+  const book = {
+    title: "Atonement",
+    author: "Ian McEwan",
+    yearOfPublication: 1998,
+    quantity: 0
+  };
+
+  expect(utils.isInStock(book)).toBe(false);
+});
+});
+
+describe("getTotalOrderPrice", () => {
+  test("multiply the price of book, by the quantity ordered, and return this price with 20% VAT added on", () => {
+    expect(utils.getTotalOrderPrice(10, 2)).toBe(24);
+  });
+}); 
